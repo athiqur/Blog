63cf8b545dff2964b639273e4734baee5713fbdb
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-11-28 12:54:39 +0530

    Add custom model manager for post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,11 +3,24 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super(PublishedManager, self)
+            .get_queryset()
+            .filter(status="published")
+        )
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ("draft", "Draft"),
         ("published", "Published"),
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
---
+++ mysite/blog/tests/test_modelmixintestcase.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from blog.models import Post
+from django.contrib.auth.models import User
+
+
+class ModelMixinTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="athiqur",
+            password="123",
+        )
+
+        self.draft_post = Post.objects.create(
+            title="Draft",
+            author=self.user,
+            body="Testing Draft",
+            status="draft",
+        )
+        self.published_post = Post.objects.create(
+            title="Published",
+            author=self.user,
+            body="Testing Published",
+            status="published",
+        )

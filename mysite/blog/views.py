691b728f35fbe791ca61e1445f35fefffb50e0c1
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-11-29 11:35:58 +0530

    Create detail view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,3 +6,15 @@ def post_list(request):
     return render(
         request, "blog/post/list.html", {"posts": Post.published.all()}
     )
+    
+
+def post_detail(request, year, month, day, post_slug):
+    post = get_object_or_404(
+        Post,
+        slug=post_slug,
+        status="published",
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(request, "blog/post/detail.html", {"post": post})
